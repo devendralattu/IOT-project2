55ec82e947c0be6cd563dcd5e873e2f0b86c4d6c
Devendra Lattu <dev12@umbc.edu>
2015-11-17 16:17:50 -0500

    added -lpthread to sensors

--- commands.sh
+++ commands.sh
@@ -1,7 +1,7 @@
 gcc -o gateway  Gateway.c -lpthread
-gcc -o mo motion.c
-gcc -o ko keychain.c
-gcc -o do door.c
+gcc -o mo motion.c -lpthread
+gcc -o ko keychain.c -lpthread
+gcc -o do door.c -lpthread
 gcc -o bo backend.c
 gnome-terminal -e ./bo
 sleep 1;
@@ -12,4 +12,3 @@ sleep 2;
 gnome-terminal -e ./ko #KeychainConfigurationFile.txt KeychainStateFile.txt
 sleep 2;
 gnome-terminal -e ./mo #MotionSensorConfigurationFile.txt MotionSensorStateFile.txt
-#/home/devendralattu/Desktop/AOS/Project2/15Nov/
