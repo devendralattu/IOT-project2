1eed26e9ea7edec595b7ea09fcc39fb36b7c4dab
Devendra Lattu <dev12@umbc.edu>
2015-11-17 19:43:30 -0500

    Vector Clock
    Used strstr

--- keychain.c
+++ keychain.c
@@ -259,11 +259,11 @@ void * connection_handler_2(void * cs2)
 	while(msglen = recv(cs_r.sock, readmsg, 2000, 0) > 0)
 	{
 		puts(readmsg);
-		if(strcmp(readmsg, "motion-") < 0)
+		if(strstr(readmsg, "motion-") != NULL)
 		{
 			vectorClock[1] = vectorClock[1] + 1;
 		}
-		if(strcmp(readmsg, "door-") < 0)
+		if(strstr(readmsg, "door-") != NULL)
 		{
 			vectorClock[2] = vectorClock[2] + 1;
 		}
