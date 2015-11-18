7d53926778f299a8e44a6d917eaceb6dd72a0a50
Devendra Lattu <dev12@umbc.edu>
2015-11-17 19:00:47 -0500

    Added security file

--- commands.sh
+++ commands.sh
@@ -1,15 +1,18 @@
-gcc -o gateway  Gateway.c -lpthread
-gcc -o mo motion.c
-gcc -o ko keychain.c
-gcc -o do door.c
 gcc -o bo backend.c
+gcc -o gateway  Gateway.c -lpthread
+gcc -o mo motion.c -lpthread
+gcc -o ko keychain.c -lpthread
+gcc -o do door.c -lpthread
+gcc -o so security.c
+
 gnome-terminal -e ./bo
 sleep 1;
 gnome-terminal -e ./gateway
+sleep 1;
+gnome-terminal -e ./so
 sleep 2;
 gnome-terminal -e ./do #DoorConfigurationFile.txt DoorStateFile.txt
 sleep 2;
 gnome-terminal -e ./ko #KeychainConfigurationFile.txt KeychainStateFile.txt
 sleep 2;
 gnome-terminal -e ./mo #MotionSensorConfigurationFile.txt MotionSensorStateFile.txt
-#/home/devendralattu/Desktop/AOS/Project2/15Nov/
